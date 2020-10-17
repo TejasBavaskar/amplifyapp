21125947eabb0cde924965413f026f448a40971f
Tejas Bavaskar <44202599+TejasBavaskar@users.noreply.github.com>
2020-10-17 13:16:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with TEJAS
+          AWS Practice.
         </a>
       </header>
     </div>
